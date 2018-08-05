0668999fef7570937203733f8e8e6f125b6102e1
wanggg360 <wanggang_sd@163.com>
2018-08-05 23:10:56 +0800

    add test

--- build/scripts/dbscripts.sql
+++ build/scripts/dbscripts.sql
@@ -2,7 +2,6 @@ DROP DATABASE IF EXISTS sampledb;
 CREATE DATABASE sampledb DEFAULT CHARACTER SET utf8;
 USE sampledb;
 
-##创建用户表
 CREATE TABLE t_user (
    user_id   INT AUTO_INCREMENT PRIMARY KEY,
    user_name VARCHAR(30),
@@ -12,7 +11,6 @@ CREATE TABLE t_user (
    last_ip  VARCHAR(23)
 )ENGINE=InnoDB; 
 
-##创建用户登录日志表
 CREATE TABLE t_login_log (
    login_log_id  INT AUTO_INCREMENT PRIMARY KEY,
    user_id   INT,
@@ -20,7 +18,6 @@ CREATE TABLE t_login_log (
    login_datetime datetime
 )ENGINE=InnoDB; 
 
-##插入初始化数据
 INSERT INTO t_user (user_name,password) 
              VALUES('admin','123456');
 COMMIT;
